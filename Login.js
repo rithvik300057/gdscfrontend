accf937550dd93e345829726e610b6adcc498108
rithvik300057 <144152751+rithvik300057@users.noreply.github.com>
2023-09-10 20:40:32 +0530

    Add files via upload

---
+++ Login.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import React from 'react';
+import './App.css';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import {GoogleLogin} from '@react-oauth/google';
+import jwt_decode from "jwt-decode";
+import ReactDOM from 'react-dom';
+// import Google from './google';
+const Login = () => {
+  return(
+  <div>
+    <GoogleOAuthProvider clientId="229751792230-mr0fpshb80j21jfucj0dui2c7hmmridl.apps.googleusercontent.com">
+    <GoogleLogin
+  onSuccess={(credentialResponse) => {
+    console.log(credentialResponse);
+  }}
+  onError={() => {
+    console.log('Login Failed');
+  }}
+ />
+</GoogleOAuthProvider>
+
+  </div>
+)
+   
+};
+export default Login;
+
+
+
+
+
+
+
+
+
+
